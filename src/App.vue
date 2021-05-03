f9bd6220cfac834c62eecd9180363a88cf21f2f8
ashley <ashleyhuntdesign@gmail.com>
2021-05-03 12:38:51 -0500

    Adding header component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
+<style>
+  @import './assets/styles/main.css';
+</style>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+<Header />
+<!-- <div class="container"> -->
+  
+<!-- </div> -->
 </template>
 
-<script lang="ts">
+<script>
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header';
 
 @Options({
   components: {
-    HelloWorld,
+    Header
   },
 })
 export default class App extends Vue {}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template> 
+    <header>
+        <img class="main-logo" src="/img/Clear_Design_mark_WHT.png" />
+        <!-- <h1>Typicals</h1> -->
+        
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+    }
+</script>
