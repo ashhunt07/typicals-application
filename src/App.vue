f68a8947affd65ae822b50249f971e406c4f67b3
ashley <ashleyhuntdesign@gmail.com>
2021-05-03 16:21:46 -0500

    Redoing folders and base-code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
-<style>
-  @import './assets/styles/main.css';
-  
-</style>
-
 <template>
-
-<Header />
-<!-- <div class="container"> -->
-  
-<!-- </div> -->
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import { Options, Vue } from 'vue-class-component';
-import Header from './components/Header';
+import HelloWorld from './components/HelloWorld.vue'
 
-
-@Options({
+export default {
+  name: 'App',
   components: {
-    Header,
-  },
-})
-export default class App extends Vue {}
+    HelloWorld
+  }
+}
 </script>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,5 @@
 import { createApp } from 'vue'
-import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-Vue.config.productionTip = false
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App).mount('#app')
